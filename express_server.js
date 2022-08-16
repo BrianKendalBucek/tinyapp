d029ef1050f0efa854622cbb0fafc84494c2c14e
BrianKendalBucek <bbucek@gmail.com>
2022-08-16 22:37:49 +0000

    some touchups

--- express_server.js
+++ express_server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);

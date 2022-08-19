3cd9428a55e2987f82d20d073ea78519042b29ac
BrianKendalBucek <bbucek@gmail.com>
2022-08-19 17:50:59 +0000

    added function to produce the 6 random

--- express_server.js
+++ express_server.js
@@ -1,17 +1,40 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+generateRandomString();
+
+////GET REQUESTS
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -22,11 +45,18 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+////POST REQUESTS
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
